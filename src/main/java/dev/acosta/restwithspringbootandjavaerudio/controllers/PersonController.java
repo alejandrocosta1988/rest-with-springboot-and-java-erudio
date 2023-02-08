d0825d5961c073d33cf28363875e91f96ebd5d96
Alejandro Costa <alejandroesfc@gmail.com>
2023-02-08 08:42:01 -0300

    sets up with person, person controller and person services

--- src/main/java/dev/acosta/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ src/main/java/dev/acosta/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package dev.acosta.restwithspringbootandjavaerudio.controllers;
 
+import dev.acosta.restwithspringbootandjavaerudio.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -8,5 +10,10 @@ import java.util.concurrent.atomic.AtomicLong;
 public class PersonController {
 
     private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private PersonServices service;
+
+    
 
 }
---
+++ src/main/java/dev/acosta/restwithspringbootandjavaerudio/services/PersonServices.java
@@ -0,0 +1,14 @@
+package dev.acosta.restwithspringbootandjavaerudio.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+}
