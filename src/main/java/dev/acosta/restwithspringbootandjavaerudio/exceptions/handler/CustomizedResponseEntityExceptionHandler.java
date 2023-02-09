6d81aba139fe00c0c683a1ff28b3d491aecdb362
Alejandro Costa <alejandroesfc@gmail.com>
2023-02-09 09:05:14 -0300

    sets not found exception

--- src/main/java/dev/acosta/restwithspringbootandjavaerudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/dev/acosta/restwithspringbootandjavaerudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,7 @@
 package dev.acosta.restwithspringbootandjavaerudio.exceptions.handler;
 
 import dev.acosta.restwithspringbootandjavaerudio.exceptions.ExceptionResponse;
-import dev.acosta.restwithspringbootandjavaerudio.exceptions.UnsupportedMathOperationException;
+import dev.acosta.restwithspringbootandjavaerudio.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,11 +25,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    //Tratamento da exceção criada (UnsupportedMathOperation)
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+    //Tratamento da exceção criada (ResourceNotFoundException)
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
 
