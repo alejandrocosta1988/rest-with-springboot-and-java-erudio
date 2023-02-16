53c7baae142b2003220c364189f6b034bd79789c
Alejandro Costa <alejandroesfc@gmail.com>
2023-02-16 17:42:31 -0300

    adds migrations with flywaydb

--- src/main/java/dev/acosta/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ src/main/java/dev/acosta/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping(value = "/person/v1")
 public class PersonController {
 
     private final AtomicLong counter = new AtomicLong();
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`)
+    VALUES
+        (1, 'Jana', 'Santana', 'Valença', 'Female'),
+        (2, 'Alejandro', 'Costa', 'Salvador', 'Male');
